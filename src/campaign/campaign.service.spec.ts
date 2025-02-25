a811953979fe783474397343e5f0e26cab699c31
David Oliveira <dedepco@gmail.com>
2025-02-24 22:38:26 -0300

    implement campaign service with findAll method and corresponding tests

--- src/campaign/campaign.service.spec.ts
+++ src/campaign/campaign.service.spec.ts
@@ -1,18 +1,85 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CampaignService } from './campaign.service';
+import { Campaign, CampaignStatus } from './entities/campaign.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('CampaignService', () => {
   let service: CampaignService;
+  let repository: Repository<Campaign>;
+
+  const mockCampaigns: Campaign[] = [
+    {
+      id: 1,
+      name: 'Campanha 1',
+      createdAt: new Date(),
+      startDate: new Date(),
+      endDate: new Date(new Date().getTime() + 1000 * 60 * 60 * 24),
+      status: CampaignStatus.ACTIVE,
+      category: 'Marketing',
+    },
+    {
+      id: 2,
+      name: 'Campanha 2',
+      createdAt: new Date(),
+      startDate: new Date(),
+      endDate: new Date(new Date().getTime() + 1000 * 60 * 60 * 24),
+      status: CampaignStatus.PAUSED,
+      category: 'Financeiro',
+    },
+    {
+      id: 3,
+      name: 'Campanha 3',
+      createdAt: new Date(),
+      startDate: new Date(),
+      endDate: new Date(new Date().getTime() + 1000 * 60 * 60 * 24),
+      status: CampaignStatus.EXPIRED,
+      category: 'Educação',
+    },
+    {
+      id: 4,
+      name: 'Campanha 4',
+      createdAt: new Date(),
+      startDate: new Date(),
+      endDate: new Date(new Date().getTime() + 1000 * 60 * 60 * 24),
+      status: CampaignStatus.ACTIVE,
+      category: 'Saúde',
+    },
+    {
+      id: 5,
+      name: 'Campanha 5',
+      createdAt: new Date(),
+      startDate: new Date(),
+      endDate: new Date(new Date().getTime() + 1000 * 60 * 60 * 24),
+      status: CampaignStatus.ACTIVE,
+      category: 'Esportes',
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CampaignService],
+      providers: [
+        CampaignService,
+        {
+          provide: getRepositoryToken(Campaign),
+          useValue: {
+            find: jest.fn().mockResolvedValue(mockCampaigns),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CampaignService>(CampaignService);
+    repository = module.get<Repository<Campaign>>(getRepositoryToken(Campaign));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return an array of campaigns', async () => {
+    const campaigns = await service.findAll();
+    expect(campaigns).toHaveLength(5);
+    expect(campaigns).toEqual(mockCampaigns);
   });
 });
--- src/campaign/campaign.service.ts
+++ src/campaign/campaign.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Campaign } from './entities/campaign.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CampaignService {}
+export class CampaignService {
+    constructor(
+        @InjectRepository(Campaign)
+        private readonly campaignRepository: Repository<Campaign>,
+      ) {}
+
+      async findAll(): Promise<Campaign[]> {
+        return this.campaignRepository.find();
+      }
+}
