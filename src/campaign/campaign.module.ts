165c10134723dff458a7d6653361fd86caa0842c
David Oliveira <dedepco@gmail.com>
2025-02-24 23:19:40 -0300

    feat: add CampaignController with CRUD endpoints and integrate Swagger for API documentation

--- src/campaign/campaign.module.ts
+++ src/campaign/campaign.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CampaignService } from './campaign.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Campaign } from './entities/campaign.entity';
 
 @Module({
-  providers: [CampaignService]
+  providers: [CampaignService],
+  imports: [TypeOrmModule.forFeature([Campaign])],
+
+  exports: [CampaignService],
 })
 export class CampaignModule {}
