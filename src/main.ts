8f326a0e1fbce47273643c1de901ed59880b8e08
David Oliveira <dedepco@gmail.com>
2025-02-28 15:27:37 -0300

    feat: add Husky pre-commit and pre-push hooks for linting, type-checking, and testing; enhance main.ts with logger support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(Logger));
 
   setupSwagger(app);
 
